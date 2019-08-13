629683ac1a1d3c2b55537b41e1d160f219083b45
Pranav Surampudi <41884998+Pranav-20186017@users.noreply.github.com>
2019-08-13 23:25:32 +0530

    shell file modified

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
