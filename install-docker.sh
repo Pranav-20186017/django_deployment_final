7370b84b7d934c4546cf88d9467dc3a85e17578b
Pranav Surampudi <41884998+Pranav-20186017@users.noreply.github.com>
2019-08-25 22:29:37 +0530

    Automated building docker image

--- install-docker.sh
+++ install-docker.sh
@@ -15,4 +15,4 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
-
+sudo docker-compose up --build
